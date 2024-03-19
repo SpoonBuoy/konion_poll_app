49da8d3e91a5fb6995c8f7ef093cf93868d8ddc0
Arsalan Rashid <arsalanrashidthoker@gmail.com>
2024-03-19 06:17:09 +0530

    add timer

--- src/components/ActivePoll.vue
+++ src/components/ActivePoll.vue
@@ -2,6 +2,7 @@
 import router from '@/router'
 import PollUserInfo from '../components/PollUserInfo.vue'
 import ActionButton from './ActionButton.vue'
+import CountDown from './CountDown.vue'
 import StateBadge from './StateBadge.vue'
 
 const details = () => {
@@ -14,7 +15,7 @@ const details = () => {
     class="flex flex-col justify-start gap-10 items-center border-2 border-black rounded-lg py-5 w-[98%] md:w-[80%] lg:w-[60%] relative"
   >
     <StateBadge message="Live" icon="bi bi-broadcast" />
-    <div class="flex flex-row items-center gap-6 sm:gap-7 md:gap-10 lg:gap-16 mt-10">
+    <div class="flex flex-row items-center gap-6 sm:gap-7 md:gap-10 lg:gap-16 mt-10 mb-10">
       <PollUserInfo
         src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
         name="Chris Allen"
@@ -23,7 +24,17 @@ const details = () => {
       <h1>VS</h1>
       <PollUserInfo name="Donald Trump" votes="10" />
     </div>
-
-    <ActionButton text="Enter" @click="details" />
+    <div class="flex flex-row absolute bottom-0 rounded-b-lg w-full bg-green-100 justify-between">
+      <CountDown
+        text="Enter"
+        @click="details"
+        class="w-[30%] shadow-white opacity-50 bg-inherit hover:opacity-70"
+      />
+      <ActionButton
+        text="Enter"
+        @click="details"
+        class="w-[30%] opacity-50 shadow-white bg-inherit hover:opacity-70"
+      />
+    </div>
   </div>
 </template>
---
+++ src/components/CountDown.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const hr = ref(10)
+const min = ref(10)
+const sec = ref(10)
+setInterval(() => {
+  if (sec.value == 0) {
+    if (min.value == 0) {
+      hr.value--
+      min.value = 60
+    }
+    min.value--
+    sec.value = 60
+  }
+  sec.value--
+}, 1000)
+</script>
+
+<template>
+  <span class="countdown flex flex-row font-mono text-sm font-bold text-black m-2 ml-4">
+    <span class="hrs" style="--value: 10">{{ hr < 10 ? `0${hr}` : hr }}</span
+    >: <span class="mins" style="--value: 24">{{ min < 10 ? `0${min}` : min }}</span
+    >:
+    <span class="secs" style="--value: 3">{{ sec < 10 ? `0${sec}` : sec }}</span>
+  </span>
+</template>
